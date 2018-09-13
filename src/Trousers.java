fd32c9688ffe41753503b731b35a7ba5b0b5c4b5
Michal Madejski <michalmadejski00@gmail.com>
2018-09-13 18:52:01 +0200

    Commit po 1.2

--- src/Trousers.java
+++ src/Trousers.java
@@ -12,6 +12,21 @@ public class Trousers {
     double discount;
     double price;
 
+    Trousers (int size, String fabric, String color, String forGender, String style, String producer, double price, String currency, boolean promotion, double discount) {
 
+        this.size = size;
+        this.fabric = fabric;
+        this.color = color;
+        this.forGender = forGender;
+        this.style = style;
+        this.producer = producer;
+        this.discount = discount;
+        this.promotion = promotion;
+        this.price = price;
+        this.currency = currency;
+
+
+
+    }
 
 }
